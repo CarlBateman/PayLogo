92f4d80aa9c0fefbba13029535dfd259d20c4f70
Carl Bateman <carlbateman@hotmail.com>
2019-11-01 21:49:14 +0000

    More Promises Trying to put common code in own module

--- widget/common.js
+++ widget/common.js
@@ -1,4 +1,11 @@
-﻿function setRandom(el) {
+﻿//function ms3Common() {
+//}
+//ns.test = function () {
+//  console.log("yeah");
+//};
+
+
+function setRandom(el) {
   let sib = el.parentNode.firstChild;
 
   while (sib) {
@@ -41,4 +48,71 @@ function showDownloadDialog() {
       closeModal();
     }
   };
+}
+
+function insertHTML() {
+  var section = document.createElement('div');
+  section.setAttribute('id', "m3s-wgl-widget");
+  section.innerHTML = `
+        <div class="m3s-wgl-ui-panel">
+          <div class="m3s-wgl-ui-header">
+            <p id="company_name">Myriad 3D Studio</p>
+            <p id="gizmo_name">WebGL Widget</p>
+          </div>
+
+          <div class="m3s-wgl-ui-mainbody">
+            <div class="m3s-wgl-ui-cell"></div>
+            <div class="m3s-wgl-ui-cell">
+              <label class="m3s-wgl-ui-label" for="radius">Radius</label>
+              <button class="m3s-wgl-ui-button" onclick="setRandom(this)">Randomise</button>
+              <input type="range" min="-2" max="4" step="0.01" value="0" class="m3s-slider" id="radius">
+            </div>
+            <div class="m3s-wgl-ui-cell">
+              <label class="m3s-wgl-ui-label" for="flip">Flip</label>
+              <button class="m3s-wgl-ui-button" onclick="setRandom(this)">Randomise</button>
+              <input type="range" min="-3.14159" max="3.14159" step="0.4488" class="m3s-slider" id="flip">
+            </div>
+            <div class="m3s-wgl-ui-cell">
+              <label class="m3s-wgl-ui-label" for="spin">Spin</label>
+              <button class="m3s-wgl-ui-button" onclick="setRandom(this)">Randomise</button>
+              <input type="range" min="-3.14159" max="3.14159" step="0.0349" value="0" class="m3s-slider" id="spin">
+            </div>
+            <div class="m3s-wgl-ui-cell"></div>
+          </div>
+
+          <div class="m3s-wgl-ui-footer">
+            <button class="m3s-wgl-button" onclick="showDownloadDialog()">Download</button>
+          </div>
+        </div>
+
+        <div id="m3s-wgl-renderOutput">
+          <canvas id="m3s-wgl-renderCanvas" touch-action="none"></canvas>
+        </div>
+
+  <!-- The Modal -->
+  <div id="myModal" class="modal">
+
+    <!-- Modal content -->
+    <div class="modal-content">
+      <div class="modal-header">
+        <span class="close">&times;</span>
+        <h2>Download Model</h2>
+      </div>
+      <div class="modal-body">
+        <p>Note: The model will be downloaded as two files: <br />
+        <span id="fixed">&emsp;widget.gltf
+        <br />
+        &emsp;widget.bin</span></p>
+        <p>Type the name you'd like for your model below:
+        <input type="text" name="fileName" maxlength="512" id="m3s-wgl-filename" placeholder="widget"/></p>
+      </div>
+      <div class="modal-footer">
+        <button class="m3s-wgl-button half-width" onclick="download()">Download</button>
+        &emsp;
+        <button class="m3s-wgl-button half-width" onclick="closeModal()">Cancel</button>
+      </div>
+    </div>
+  </div>`;
+
+  document.getElementById("LogoGenerator").appendChild(section);
 }
--- widget/m3s-widget.js
+++ widget/m3s-widget.js
@@ -1,34 +1,39 @@
 ﻿window.addEventListener('DOMContentLoaded', function () {
   let script = document.getElementById('m3s-wgl-widget');
-  var path = script.src.split('?')[0];      // remove any ?query
+  var path = script.src.split('?')[0];
   var mydir = path.split('/').slice(0, -1).join('/') + '/';
 
   insertStyle(mydir + "css/wgl-widget.css");
   insertStyle(mydir + "css/modal.css");
 
-  insertHTML();
-  insertScript(mydir + "common.js");
+
+
+  Promise.resolve()
+    .then(loadCommon)
+    .then(() => { insertHTML(); })
+    //.then(insertHTML)
+    .then(() => { })
+    ;
+
+  //loadCommon()
+  //  .then(() => { insertHTML(); });
+
+  //insertHTML();
+  //insertScript(mydir + "common.js");
   insertScript(mydir + "m3s-babylon.js");
-
-  function run() { doWidgetBabylon(mydir); }
-
-  //loadBabylonPromise()
-  //  .then(() => { waitFor(isBabylonLoaded); })
-  //  .then(loadExtensions)
-  //  .then(run);
 
   loadBabylon()
     .then(loadExtensions)
-    .then(run);
+    .then(() => { doWidgetBabylon(mydir); });
 
   function isCommonLoaded() { return typeof setRandom !== "undefined"; }
   function isBabylonLoaded() { return typeof BABYLON !== "undefined"; }
   function isLoaderAvailable() { return BABYLON.SceneLoader.IsPluginForExtensionAvailable(".glb"); }
 
-  function loadBabylonPromise() {
+  function loadCommon() {
     return new Promise((resolve, reject) => {
-      insertScript('https://cdn.babylonjs.com/babylon.max.js');
-      resolve();
+      insertScript(mydir + "common.js");
+      waitForPromise(resolve, reject, isCommonLoaded);
     });
   }
 
@@ -45,24 +50,6 @@
     return new Promise((resolve, reject) => {
       insertScript('https://preview.babylonjs.com/serializers/babylonjs.serializers.min.js');
       waitForPromise(resolve, reject, isLoaderAvailable);
-    });
-  }
-
-  function waitFor(condition) {
-    var wait = 0;
-
-    return new Promise((resolve, reject) => {
-      (function waitForLocal(condition) {
-        wait += 10;
-        if (wait > 100000) reject();
-
-        setTimeout(function () {
-          if (condition())
-            resolve();
-          else
-            waitForLocal(condition);
-        }, 10);
-      })(condition);
     });
   }
 
@@ -95,71 +82,5 @@
     document.head.appendChild(s);
   }
 
-  function insertHTML() {
-    var section = document.createElement('div');
-    section.setAttribute('id', "m3s-wgl-widget");
-    section.innerHTML = `
-        <div class="m3s-wgl-ui-panel">
-          <div class="m3s-wgl-ui-header">
-            <p id="company_name">Myriad 3D Studio</p>
-            <p id="gizmo_name">WebGL Widget</p>
-          </div>
-
-          <div class="m3s-wgl-ui-mainbody">
-            <div class="m3s-wgl-ui-cell"></div>
-            <div class="m3s-wgl-ui-cell">
-              <label class="m3s-wgl-ui-label" for="radius">Radius</label>
-              <button class="m3s-wgl-ui-button" onclick="setRandom(this)">Randomise</button>
-              <input type="range" min="-2" max="4" step="0.01" value="0" class="m3s-slider" id="radius">
-            </div>
-            <div class="m3s-wgl-ui-cell">
-              <label class="m3s-wgl-ui-label" for="flip">Flip</label>
-              <button class="m3s-wgl-ui-button" onclick="setRandom(this)">Randomise</button>
-              <input type="range" min="-3.14159" max="3.14159" step="0.4488" class="m3s-slider" id="flip">
-            </div>
-            <div class="m3s-wgl-ui-cell">
-              <label class="m3s-wgl-ui-label" for="spin">Spin</label>
-              <button class="m3s-wgl-ui-button" onclick="setRandom(this)">Randomise</button>
-              <input type="range" min="-3.14159" max="3.14159" step="0.0349" value="0" class="m3s-slider" id="spin">
-            </div>
-            <div class="m3s-wgl-ui-cell"></div>
-          </div>
-
-          <div class="m3s-wgl-ui-footer">
-            <button class="m3s-wgl-button" onclick="showDownloadDialog()">Download</button>
-          </div>
-        </div>
-
-        <div id="m3s-wgl-renderOutput">
-          <canvas id="m3s-wgl-renderCanvas" touch-action="none"></canvas>
-        </div>
-
-  <!-- The Modal -->
-  <div id="myModal" class="modal">
-
-    <!-- Modal content -->
-    <div class="modal-content">
-      <div class="modal-header">
-        <span class="close">&times;</span>
-        <h2>Download Model</h2>
-      </div>
-      <div class="modal-body">
-        <p>Note: The model will be downloaded as two files: <br />
-        <span id="fixed">&emsp;widget.gltf
-        <br />
-        &emsp;widget.bin</span></p>
-        <p>Type the name you'd like for your model below:
-        <input type="text" name="fileName" maxlength="512" id="m3s-wgl-filename" placeholder="widget"/></p>
-      </div>
-      <div class="modal-footer">
-        <button class="m3s-wgl-button half-width" onclick="download()">Download</button>
-        &emsp;
-        <button class="m3s-wgl-button half-width" onclick="closeModal()">Cancel</button>
-      </div>
-    </div>
-  </div>`;
-
-    document.getElementById("LogoGenerator").appendChild(section);
-  }
 
 });
