b69e91c56b3fbb214215ec1320df5278d544a94d
Carl Bateman <carlbateman@hotmail.com>
2019-11-29 17:34:31 +0000

    A bit of a tidy

--- widget/m3s-common.js
+++ widget/m3s-common.js
@@ -76,59 +76,55 @@
     section.innerHTML = `
   <div class="m3s-wgl-widget-container">
 
-    <!--div class="m3s-wgl-widget-panel"-->
+    <div class="m3s-wgl-widget-header">
+      <p id="company_name">Myriad 3D Studio</p>
+      <p id="gizmo_name">WebGL Widget</p>
+    </div>
 
-      <div class="m3s-wgl-widget-header">
-        <p id="company_name">Myriad 3D Studio</p>
-        <p id="gizmo_name">WebGL Widget</p>
+    <div class="m3s-wgl-widget-menu">
+      <div class="m3s-wgl-ui-cell">
+        <label class="m3s-wgl-ui-label" for="radius">Radius</label>
+        <button class="m3s-wgl-ui-button" onclick="m3sCommon.setRandom(this)">Randomise</button>
+        <input type="range" min="-2" max="4" step="0.01" value="0" class="m3s-slider" id="radius">
       </div>
+      <div class="m3s-wgl-ui-cell">
+        <label class="m3s-wgl-ui-label" for="flip">Flip</label>
+        <button class="m3s-wgl-ui-button" onclick="m3sCommon.setRandom(this)">Randomise</button>
+        <input type="range" min="-3.14159" max="3.14159" step="0.0349" class="m3s-slider" id="flip">
+      </div>
+      <div class="m3s-wgl-ui-cell">
+        <label class="m3s-wgl-ui-label" for="spin">Spin</label>
+        <button class="m3s-wgl-ui-button" onclick="m3sCommon.setRandom(this)">Randomise</button>
+        <input type="range" min="-6.2832" max="6.2832" step="0.0349" value="0" class="m3s-slider" id="spin">
+      </div>
+      <div class="m3s-wgl-ui-cell">
+        <label class="m3s-wgl-ui-label" for="offset">Offset</label>
+        <button class="m3s-wgl-ui-button" onclick="m3sCommon.setRandom(this)">Randomise</button>
+        <input type="range" min="-1" max="3" step="0.01" value="2" class="m3s-slider" id="offset">
+      </div>
+      <div class="m3s-wgl-ui-cell">
+        <label class="m3s-wgl-ui-label" for="hue">Colour</label>
+        <button class="m3s-wgl-ui-button" onclick="m3sCommon.setRandom(this)">Randomise</button>
+        <input type="range" min="0" max="1" step="0.01" value=".5" class="m3s-slider-hue" id="hue">
+      </div>
+      <div class="m3s-wgl-ui-cell">
+        <label class="m3s-wgl-ui-label" for="offset">Brightness</label>
+        <button class="m3s-wgl-ui-button" onclick="m3sCommon.setRandom(this)">Randomise</button>
+        <input type="range" min="0" max="4" step="0.01" value="1" class="m3s-slider" id="brightness">
+      </div>
+      <div class="m3s-wgl-ui-cell">
+        <label class="m3s-wgl-ui-label" for="offset">Number</label>
+        <button class="m3s-wgl-ui-button" onclick="m3sCommon.setRandom(this)">Randomise</button>
+        <input type="range" min="2" max="15" step="1" value="7" class="m3s-slider" id="number">
+      </div>
+    </div>
 
-      <div class="m3s-wgl-widget-menu">
-        <div class="m3s-wgl-ui-cell">
-          <label class="m3s-wgl-ui-label" for="radius">Radius</label>
-          <button class="m3s-wgl-ui-button" onclick="m3sCommon.setRandom(this)">Randomise</button>
-          <input type="range" min="-2" max="4" step="0.01" value="0" class="m3s-slider" id="radius">
-        </div>
-        <div class="m3s-wgl-ui-cell">
-          <label class="m3s-wgl-ui-label" for="flip">Flip</label>
-          <button class="m3s-wgl-ui-button" onclick="m3sCommon.setRandom(this)">Randomise</button>
-          <input type="range" min="-3.14159" max="3.14159" step="0.0349" class="m3s-slider" id="flip">
-        </div>
-        <div class="m3s-wgl-ui-cell">
-          <label class="m3s-wgl-ui-label" for="spin">Spin</label>
-          <button class="m3s-wgl-ui-button" onclick="m3sCommon.setRandom(this)">Randomise</button>
-          <input type="range" min="-6.2832" max="6.2832" step="0.0349" value="0" class="m3s-slider" id="spin">
-        </div>
-        <div class="m3s-wgl-ui-cell">
-          <label class="m3s-wgl-ui-label" for="offset">Offset</label>
-          <button class="m3s-wgl-ui-button" onclick="m3sCommon.setRandom(this)">Randomise</button>
-          <input type="range" min="-1" max="3" step="0.01" value="2" class="m3s-slider" id="offset">
-        </div>
-        <div class="m3s-wgl-ui-cell">
-          <label class="m3s-wgl-ui-label" for="hue">Colour</label>
-          <button class="m3s-wgl-ui-button" onclick="m3sCommon.setRandom(this)">Randomise</button>
-          <input type="range" min="0" max="1" step="0.01" value=".5" class="m3s-slider-hue" id="hue">
-        </div>
-        <div class="m3s-wgl-ui-cell">
-          <label class="m3s-wgl-ui-label" for="offset">Brightness</label>
-          <button class="m3s-wgl-ui-button" onclick="m3sCommon.setRandom(this)">Randomise</button>
-          <input type="range" min="0" max="4" step="0.01" value="1" class="m3s-slider" id="brightness">
-        </div>
-        <div class="m3s-wgl-ui-cell">
-          <label class="m3s-wgl-ui-label" for="offset">Number</label>
-          <button class="m3s-wgl-ui-button" onclick="m3sCommon.setRandom(this)">Randomise</button>
-          <input type="range" min="2" max="15" step="1" value="7" class="m3s-slider" id="number">
-        </div>
-      </div>
-
-      <div class="m3s-wgl-widget-footer">
-        <span class="hidden" id="playcanvas-info"> Note: At the time of development PlayCanvas doesn't support download.</span>
-        <button class="" id="download-btn" onclick="m3sCommon.showDownloadDialog()">Download</button>
-      </div>
-    <!--/div-->
+    <div class="m3s-wgl-widget-footer">
+      <span class="hidden" id="playcanvas-info"> Note: At the time of development PlayCanvas doesn't support download.</span>
+      <button class="" id="download-btn" onclick="m3sCommon.showDownloadDialog()">Download</button>
+    </div>
 
     <div class="m3s-wgl-widget-content">
-
       <canvas class="m3s-wgl-canvas" id="m3s-wgl-canvas-three" touch-action="none"></canvas>
       <canvas class="m3s-wgl-canvas" id="m3s-wgl-canvas-babylon" touch-action="none"></canvas>
       <canvas class="m3s-wgl-canvas" id="m3s-wgl-canvas-playcanvas" touch-action="none"></canvas>
@@ -151,30 +147,29 @@
   </div>
 
   <!-- The Spiel -->
-<div class="m3s-spiel">
-<br /><p class="m3s-spiel-h2">A demo WebGL Widget from <strong>Myriad 3D Studio</strong></p>
-<p><strong>By Carl Bateman</strong> (<a href="http://carlbateman.com">CarlBateman.com</a>). Have fun!</p>
-  <span id="m3s-spiel-info" class="hidden">
-    <p>This is a simple WebGL Widget based on some freelance work that failed to materialise, so here it is as a portfolio piece.<br />
-    It should be easy to add to any page, just include <br />
-    <code>&emsp;&emsp;&lt;script id="m3s-wgl-widget" src="m3s-widget-setup.js"&gt;&lt;/script&gt;</code><br />
-    and<br />
-    <code>&emsp;&emsp;&lt;div id="LogoGenerator"&gt; &lt;/div&gt;</code><br />
-    where you want the widget to appear.</p>
+  <div class="m3s-spiel">
+  <br /><p class="m3s-spiel-h2">A demo WebGL Widget from <strong>Myriad 3D Studio</strong></p>
+  <p><strong>By Carl Bateman</strong> (<a href="http://carlbateman.com">CarlBateman.com</a>). Have fun!</p>
+    <span id="m3s-spiel-info" class="hidden">
+      <p>This is a simple WebGL Widget based on some freelance work that failed to materialise, so here it is as a portfolio piece.<br />
+      It should be easy to add to any page, just include <br />
+      <code>&emsp;&emsp;&lt;script id="m3s-wgl-widget" src="m3s-widget-setup.js"&gt;&lt;/script&gt;</code><br />
+      and<br />
+      <code>&emsp;&emsp;&lt;div id="LogoGenerator"&gt; &lt;/div&gt;</code><br />
+      where you want the widget to appear.</p>
 
-    <p>I've implemented the widget in Three.js, Babylon.js and PlayCanvas engine. The widget defaults to Three.js but a different engine can be selected by clicking the icon in the top left.</p>
-    <p>I've tried to get the same look across engines, but not too hard as each works slightly differently and I wanted to have some indication of the differences in the final render.</p>
+      <p>I've implemented the widget in Three.js, Babylon.js and PlayCanvas engine. The widget defaults to Three.js but a different engine can be selected by clicking the icon in the top left.</p>
+      <p>I've tried to get the same look across engines, but not too hard as each works slightly differently and I wanted to have some indication of the differences in the final render.</p>
 
-    <br /><p class="m3s-spiel-h2">Some technical stuff</p>
-    <p>Code is pulled in by creating a script and setting its <code>src</code> property. I experimented with XHR and promises; XHR hides the source file names, promises felt a bit clunky,</p>
-    <p><code>setTimeout</code> is used to check whether the code has loaded. I tried using <code>onload</code>, but felt the delay before an error occurred was a too long.</p>
-    <!--p>Different web pages can be selected to get a feel for how the widget might look in different settings. I originally did this for debugging purposes.</p-->
-    <p>NB I use the <code>&lt;button&gt;</code> tag for the button not a class. You'll need to style accordingly.</p><br />
-    <p id="m3s-spiel-less" onclick='document.getElementById("m3s-spiel-info").classList.add("hidden"); document.getElementById("m3s-spiel-more").classList.remove("hidden");'><a>Less info...</a></p>
-  </span>
-  <p id="m3s-spiel-more" onclick='document.getElementById("m3s-spiel-info").classList.remove("hidden"); document.getElementById("m3s-spiel-more").classList.add("hidden");'><a>More info...</a></p>
-</div>
-
+      <br /><p class="m3s-spiel-h2">Some technical stuff</p>
+      <p>Code is pulled in by creating a script and setting its <code>src</code> property. I experimented with XHR and promises; XHR hides the source file names, promises felt a bit clunky,</p>
+      <p><code>setTimeout</code> is used to check whether the code has loaded. I tried using <code>onload</code>, but felt the delay before an error occurred was a too long.</p>
+      <!--p>Different web pages can be selected to get a feel for how the widget might look in different settings. I originally did this for debugging purposes.</p-->
+      <p>NB I use the <code>&lt;button&gt;</code> tag for the button not a class. You'll need to style accordingly.</p><br />
+      <p id="m3s-spiel-less" onclick='document.getElementById("m3s-spiel-info").classList.add("hidden"); document.getElementById("m3s-spiel-more").classList.remove("hidden");'><a>Less info...</a></p>
+    </span>
+    <p id="m3s-spiel-more" onclick='document.getElementById("m3s-spiel-info").classList.remove("hidden"); document.getElementById("m3s-spiel-more").classList.add("hidden");'><a>More info...</a></p>
+  </div>
 
 
   <!-- The Modal -->
